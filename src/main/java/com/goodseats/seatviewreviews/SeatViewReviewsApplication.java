43bfac46c7b28ad0d0cf1bbc8c4e043aad8f4912
권성준 <tjdwns1865@naver.com>
2023-04-21 14:51:42 +0900

    Init: 프로젝트 시작

---
+++ src/main/java/com/goodseats/seatviewreviews/SeatViewReviewsApplication.java
@@ -0,0 +1,13 @@
+package com.goodseats.seatviewreviews;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeatViewReviewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeatViewReviewsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goodseats/seatviewreviews/SeatViewReviewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goodseats.seatviewreviews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeatViewReviewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
