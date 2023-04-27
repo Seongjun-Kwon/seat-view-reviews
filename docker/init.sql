9ae1f609c91e12bc8a4759cf7f8a512e7eb873c8
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-04-28 04:04:56 +0900

    Fix: init.sql 에 PK 가 auto increment 되도록 수정 (#19)

--- docker/init.sql
+++ docker/init.sql
@@ -1,29 +1,29 @@
 CREATE TABLE `member`
 (
-    `id`              BIGINT      NOT NULL,
-    `login_email`        VARCHAR(50) NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    `login_email`     VARCHAR(50) NOT NULL UNIQUE,
     `password`        VARCHAR(64) NOT NULL,
-    `nickname`        VARCHAR(10) NOT NULL,
+    `nickname`        VARCHAR(10) NOT NULL UNIQUE,
     `point`           INT         NOT NULL,
     `created_at`      TIMESTAMP   NOT NULL,
     `last_updated_at` TIMESTAMP   NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP   NULL
 );
 
 CREATE TABLE `stadium`
 (
-    `id`              BIGINT       NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `name`            VARCHAR(50)  NOT NULL,
     `address`         VARCHAR(255) NOT NULL,
     `home_team`       VARCHAR(50)  NOT NULL,
     `created_at`      TIMESTAMP    NOT NULL,
     `last_updated_at` TIMESTAMP    NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP    NULL
 );
 
 CREATE TABLE `seat`
 (
-    `id`              BIGINT      NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `stadium_id`      BIGINT      NOT NULL,
     `section`         VARCHAR(10) NOT NULL,
     `price`           INT         NOT NULL,
@@ -31,24 +31,24 @@ CREATE TABLE `seat`
     `average_score`   DECIMAL     NOT NULL,
     `created_at`      TIMESTAMP   NOT NULL,
     `last_updated_at` TIMESTAMP   NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP   NULL
 );
 
 CREATE TABLE `point_log`
 (
-    `id`              BIGINT      NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `member_id`       BIGINT      NOT NULL,
     `used_amount`     INT         NOT NULL,
     `used_field`      VARCHAR(30) NOT NULL,
     `remaining_point` INT         NOT NULL,
     `created_at`      TIMESTAMP   NOT NULL,
     `last_updated_at` TIMESTAMP   NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP   NULL
 );
 
 CREATE TABLE `review`
 (
-    `id`              BIGINT        NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `member_id`       BIGINT        NOT NULL,
     `seat_id`         BIGINT        NOT NULL,
     `title`           VARCHAR(50)   NOT NULL,
@@ -57,34 +57,34 @@ CREATE TABLE `review`
     `hits`            INT           NOT NULL,
     `created_at`      TIMESTAMP     NOT NULL,
     `last_updated_at` TIMESTAMP     NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP     NULL
 );
 
 CREATE TABLE `comment`
 (
-    `id`              BIGINT        NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `member_id`       BIGINT        NOT NULL,
     `review_id`       BIGINT        NOT NULL,
     `content`         VARCHAR(1000) NOT NULL,
     `created_at`      TIMESTAMP     NOT NULL,
     `last_updated_at` TIMESTAMP     NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP     NULL
 );
 
 CREATE TABLE `review_like`
 (
-    `id`              BIGINT      NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `member_id`       BIGINT      NOT NULL,
     `review_id`       BIGINT      NOT NULL,
     `like_type`       VARCHAR(10) NOT NULL,
     `created_at`      TIMESTAMP   NOT NULL,
     `last_updated_at` TIMESTAMP   NOT NULL,
-    `deleted_at`      TIMESTAMP NULL
+    `deleted_at`      TIMESTAMP   NULL
 );
 
 CREATE TABLE `image`
 (
-    `id`              BIGINT       NOT NULL,
+    `id`              BIGINT AUTO_INCREMENT PRIMARY KEY,
     `image_url`       VARCHAR(500) NOT NULL,
     `image_type`      VARCHAR(30)  NOT NULL,
     `reference_id`    BIGINT       NOT NULL,
@@ -94,34 +94,4 @@ CREATE TABLE `image`
     `last_updated_at` TIMESTAMP    NOT NULL,
     `deleted_at`      TIMESTAMP    NOT NULL
 );
-
-ALTER TABLE `member`
-    ADD CONSTRAINT `PK_MEMBER` PRIMARY KEY (`id`);
-
-ALTER TABLE 'member'
-    ADD CONSTRAINT `UK1_MEMBER` UNIQUE ('login_email');
-
-ALTER TABLE 'member'
-    ADD CONSTRAINT `UK2_MEMBER` UNIQUE ('nickname');
-
-ALTER TABLE `stadium`
-    ADD CONSTRAINT `PK_STADIUM` PRIMARY KEY (`id`);
-
-ALTER TABLE `seat`
-    ADD CONSTRAINT `PK_SEAT` PRIMARY KEY (`id`);
-
-ALTER TABLE `point_log`
-    ADD CONSTRAINT `PK_POINT_LOG` PRIMARY KEY (`id`);
-
-ALTER TABLE `review`
-    ADD CONSTRAINT `PK_REVIEW` PRIMARY KEY (`id`);
-
-ALTER TABLE `comment`
-    ADD CONSTRAINT `PK_COMMENT` PRIMARY KEY (`id`);
-
-ALTER TABLE `review_like`
-    ADD CONSTRAINT `PK_REVIEW_LIKE` PRIMARY KEY (`id`);
-
-ALTER TABLE `image`
-    ADD CONSTRAINT `PK_IMAGE` PRIMARY KEY (`id`);
 
