3e6638330af1d8f2b22d2b362bafd7152d27b4e6
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-04-28 04:19:05 +0900

    Fix: 회원가입 요청 DTO 의 로그인 이메일 필드에 @Email 어노테이션 추가 (#20)

--- src/main/java/com/goodseats/seatviewreviews/domain/member/model/dto/MemberSignUpRequest.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/member/model/dto/MemberSignUpRequest.java
@@ -1,11 +1,12 @@
 package com.goodseats.seatviewreviews.domain.member.model.dto;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 import org.hibernate.validator.constraints.Length;
 
 public record MemberSignUpRequest(
-		@NotBlank @Length(min = 3, max = 20) String loginEmail,
+		@NotBlank @Length(min = 3, max = 20) @Email String loginEmail,
 		@NotBlank @Length(min = 8, max = 20) String password,
 		@NotBlank @Length(max = 10) String nickname
 ) {
