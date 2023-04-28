abf9e6d2c72825f93039f216ff388078bc9657ae
권성준 <tjdwns1865@naver.com>
2023-04-29 00:47:55 +0900

    Feat: 로그인 기능 구현

--- src/main/java/com/goodseats/seatviewreviews/domain/member/service/MemberService.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/member/service/MemberService.java
@@ -1,10 +1,14 @@
 package com.goodseats.seatviewreviews.domain.member.service;
 
+import static com.goodseats.seatviewreviews.common.error.exception.ErrorCode.*;
+
 import org.springframework.stereotype.Service;
 
+import com.goodseats.seatviewreviews.common.error.exception.AuthenticationException;
 import com.goodseats.seatviewreviews.common.error.exception.DuplicatedException;
-import com.goodseats.seatviewreviews.common.error.exception.ErrorCode;
 import com.goodseats.seatviewreviews.domain.member.mapper.MemberMapper;
+import com.goodseats.seatviewreviews.domain.member.model.dto.AuthenticationDTO;
+import com.goodseats.seatviewreviews.domain.member.model.dto.MemberLoginRequest;
 import com.goodseats.seatviewreviews.domain.member.model.dto.MemberSignUpRequest;
 import com.goodseats.seatviewreviews.domain.member.model.entity.Member;
 import com.goodseats.seatviewreviews.domain.member.repository.MemberRepository;
@@ -28,15 +32,22 @@ public class MemberService {
 		return savedMember.getId();
 	}
 
+	public AuthenticationDTO login(MemberLoginRequest memberLoginRequest) {
+		return memberRepository.findByLoginEmail(memberLoginRequest.loginEmail())
+				.filter(member -> passwordEncoder.isMatch(memberLoginRequest.password(), member.getPassword()))
+				.map(member -> new AuthenticationDTO(member.getId(), member.getMemberAuthority()))
+				.orElseThrow(() -> new AuthenticationException(UNAUTHORIZED));
+	}
+
 	private void validateDuplicateEmail(String loginEmail) {
 		if (memberRepository.existsByLoginEmail(loginEmail)) {
-			throw new DuplicatedException(ErrorCode.DUPLICATED_ID);
+			throw new DuplicatedException(DUPLICATED_ID);
 		}
 	}
 
 	private void validateDuplicateNickname(String nickname) {
 		if (memberRepository.existsByNickname(nickname)) {
-			throw new DuplicatedException(ErrorCode.DUPLICATED_NICKNAME);
+			throw new DuplicatedException(DUPLICATED_NICKNAME);
 		}
 	}
 }
