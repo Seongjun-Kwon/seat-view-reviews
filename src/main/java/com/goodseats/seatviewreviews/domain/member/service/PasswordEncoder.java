832ef9c64ec3d28789b1e6e4a8507001929b47a6
권성준 <tjdwns1865@naver.com>
2023-04-29 00:35:06 +0900

    Add: PasswordEncoder 에 isMatch 메서드 추가
    - 입력 비밀번호와 암호화된 비밀번호가 매칭되는지 확인하는 메서드임

--- src/main/java/com/goodseats/seatviewreviews/domain/member/service/PasswordEncoder.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/member/service/PasswordEncoder.java
@@ -3,4 +3,6 @@ package com.goodseats.seatviewreviews.domain.member.service;
 public interface PasswordEncoder {
 
 	String encode(String password);
+
+	boolean isMatch(String password, String encodedPassword);
 }
