4c459ed446d84be4bb53d1ffafd2de697b96dc9a
권성준 <tjdwns1865@naver.com>
2023-04-29 23:16:30 +0900

    Refactor: 세션 인증 객체의 키를 상수로 분리

---
+++ src/main/java/com/goodseats/seatviewreviews/common/security/SessionConstant.java
@@ -0,0 +1,10 @@
+package com.goodseats.seatviewreviews.common.security;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SessionConstant {
+
+	public static final String LOGIN_MEMBER_INFO = "loginMemberInfo";
+}
