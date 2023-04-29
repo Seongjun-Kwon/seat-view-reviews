4a42d4651ea6add338df6a5a8b8bcfab01da0b2f
권성준 <tjdwns1865@naver.com>
2023-04-30 00:59:28 +0900

    Test: 로그인 API 서비스단 단위테스트 작성

--- src/test/java/com/goodseats/seatviewreviews/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/goodseats/seatviewreviews/domain/member/service/MemberServiceTest.java
@@ -4,6 +4,8 @@ import static com.goodseats.seatviewreviews.common.error.exception.ErrorCode.*;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,9 +15,13 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.goodseats.seatviewreviews.common.error.exception.AuthenticationException;
 import com.goodseats.seatviewreviews.common.error.exception.DuplicatedException;
+import com.goodseats.seatviewreviews.domain.member.model.dto.AuthenticationDTO;
+import com.goodseats.seatviewreviews.domain.member.model.dto.MemberLoginRequest;
 import com.goodseats.seatviewreviews.domain.member.model.dto.MemberSignUpRequest;
 import com.goodseats.seatviewreviews.domain.member.model.entity.Member;
+import com.goodseats.seatviewreviews.domain.member.model.vo.MemberAuthority;
 import com.goodseats.seatviewreviews.domain.member.repository.MemberRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -39,12 +45,18 @@ class MemberServiceTest {
 			"Jerome"
 	);
 
+	private MemberLoginRequest loginRequest = new MemberLoginRequest(
+			memberSignUpRequest.loginEmail(),
+			memberSignUpRequest.password()
+	);
+
+	private String encodedPassword = BCrypt.hashpw(memberSignUpRequest.password(), BCrypt.gensalt());
+	private Long memberId = 1L;
+
 	@Test
 	@DisplayName("Success - 회원가입에 성공한다")
 	void signUpSuccess() {
 		// given
-		String encodedPassword = BCrypt.hashpw("password", BCrypt.gensalt());
-		Long memberId = 1L;
 		when(memberRepository.existsByLoginEmail(memberSignUpRequest.loginEmail())).thenReturn(false);
 		when(memberRepository.existsByNickname(memberSignUpRequest.nickname())).thenReturn(false);
 		when(passwordEncoder.encode(memberSignUpRequest.password())).thenReturn(encodedPassword);
@@ -100,6 +112,65 @@ class MemberServiceTest {
 			verify(passwordEncoder, times(0)).encode(memberSignUpRequest.password());
 			verify(memberRepository, times(0)).save(any(Member.class));
 			verify(savedMember, times(0)).getId();
+		}
+	}
+
+	@Test
+	@DisplayName("Success - 로그인에 성공한다.")
+	void loginSuccess() {
+		// given
+		when(memberRepository.findByLoginEmail(loginRequest.loginEmail())).thenReturn(Optional.of(savedMember));
+		when(savedMember.getPassword()).thenReturn(encodedPassword);
+		when(passwordEncoder.isMatch(loginRequest.password(), encodedPassword)).thenReturn(true);
+		when(savedMember.getId()).thenReturn(memberId);
+		when(savedMember.getMemberAuthority()).thenReturn(MemberAuthority.USER);
+
+		// when
+		AuthenticationDTO authenticationDTO = memberService.login(loginRequest);
+
+		// then
+		verify(memberRepository).findByLoginEmail(loginRequest.loginEmail());
+		verify(savedMember).getPassword();
+		verify(passwordEncoder).isMatch(loginRequest.password(), encodedPassword);
+		verify(savedMember).getId();
+		verify(savedMember).getMemberAuthority();
+		assertThat(authenticationDTO.memberId()).isEqualTo(memberId);
+		assertThat(authenticationDTO.memberAuthority()).isEqualTo(MemberAuthority.USER);
+	}
+
+	@Nested
+	@DisplayName("loginFail")
+	class LoginFail {
+
+		@Test
+		@DisplayName("Fail - 잘못된 아이디를 입력하여 로그인에 실패한다.")
+		void loginFailByWrongLoginEmail() {
+			// given
+			when(memberRepository.findByLoginEmail(loginRequest.loginEmail())).thenReturn(Optional.empty());
+
+			// when & then
+			assertThatThrownBy(() -> memberService.login(loginRequest))
+					.isExactlyInstanceOf(AuthenticationException.class)
+					.hasMessage(UNAUTHORIZED.getMessage());
+			verify(memberRepository).findByLoginEmail(loginRequest.loginEmail());
+		}
+
+		@Test
+		@DisplayName("Fail - 잘못된 비밀번호를 입력하여 로그인에 실패한다.")
+		void loginFailByWrongPassword() {
+			// given
+			String wrongPassword = "wrongPassword";
+			when(memberRepository.findByLoginEmail(loginRequest.loginEmail())).thenReturn(Optional.of(savedMember));
+			when(savedMember.getPassword()).thenReturn(wrongPassword);
+			when(passwordEncoder.isMatch(loginRequest.password(), wrongPassword)).thenReturn(false);
+
+			// when & then
+			assertThatThrownBy(() -> memberService.login(loginRequest))
+					.isExactlyInstanceOf(AuthenticationException.class)
+					.hasMessage(UNAUTHORIZED.getMessage());
+			verify(memberRepository).findByLoginEmail(loginRequest.loginEmail());
+			verify(savedMember).getPassword();
+			verify(passwordEncoder).isMatch(loginRequest.password(), wrongPassword);
 		}
 	}
 }
