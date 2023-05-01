0e29e03a1545b7ccc11058bed09527d6f637e98e
권성준 <tjdwns1865@naver.com>
2023-05-02 05:43:39 +0900

    Add: 잠실 야구장 좌석 등급 데이터 SQL 추가

---
+++ docker/jamsil-seat-grade.sql
@@ -0,0 +1,29 @@
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('프리미엄', '주중 80,000 / 주말 80,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('테이블', '주중 47,000 / 주말 53,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('익사이팅', '주중 25,000 / 주말 30,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('블루', '주중 20,000 / 주말 22,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('블루 휠체어', '주중 10,000 / 주말 11,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('오렌지', '주중 18,000 / 주말 20,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('레드', '주중 16,000 / 주말 18,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('레드 휠체어', '주중 8,000 / 주말 9,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('네이비', '주중 13,000 / 주말 15,000', 1, NOW(), null, NOW());
+
+INSERT INTO seat_grade (name, price_info, stadium_id, created_at, deleted_at, last_updated_at)
+VALUES ('외야 그린', '주중 성인-8,000, 청소년-6,000, 어린이-4,000 / 주말 성인-9,000, 청소년-7,000, 어린이-4,500', 1, NOW(), null, NOW());
