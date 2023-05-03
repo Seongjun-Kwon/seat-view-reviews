78a10d96c4a3b0fed6b1cf3a5b6b60c034ab0bef
권성준 <tjdwns1865@naver.com>
2023-05-04 05:01:33 +0900

    Refactor: 하드 코딩되어있던 좌석 구역 SQL 들을 스토어드 프로시저 사용하여 정리

--- docker/jamsil-seat-section.sql
+++ docker/jamsil-seat-section.sql
@@ -1,202 +1,45 @@
+DROP PROCEDURE IF EXISTS insert_seat_sections;
+DELIMITER //
+CREATE PROCEDURE insert_seat_sections(IN grade_id INT, IN start_section INT, IN last_section INT)
+BEGIN
+    DECLARE i INT DEFAULT start_section;
+    WHILE i <= last_section
+        DO
+            INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
+            VALUES (CONCAT(i), grade_id, NOW(), null, NOW());
+            SET i = i + 1;
+        END WHILE;
+END //
+DELIMITER ;
+
 INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
 VALUES ('프리미엄', 1, NOW(), null, NOW());
 
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('110', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('111', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('112', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('113', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('212', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('213', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('214', 2, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('215', 2, NOW(), null, NOW());
+CALL insert_seat_sections(2, 110, 113);
+CALL insert_seat_sections(2, 212, 215);
 
 INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
 VALUES ('홈', 3, NOW(), null, NOW());
-
 INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
 VALUES ('원정', 3, NOW(), null, NOW());
 
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('107', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('108', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('109', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('209', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('210', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('211', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('114', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('115', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('116', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('216', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('217', 4, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('218', 4, NOW(), null, NOW());
+CALL insert_seat_sections(4, 107, 109);
+CALL insert_seat_sections(4, 209, 211);
+CALL insert_seat_sections(4, 114, 116);
+CALL insert_seat_sections(4, 216, 218);
 
 INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
 VALUES ('109', 5, NOW(), null, NOW());
-
 INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
 VALUES ('114', 5, NOW(), null, NOW());
 
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('205', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('206', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('207', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('208', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('219', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('220', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('221', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('222', 6, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('101', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('102', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('103', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('104', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('105', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('106', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('201', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('202', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('203', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('204', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('117', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('118', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('119', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('120', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('121', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('122', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('223', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('224', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('225', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('226', 7, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('101', 8, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('102', 8, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('121', 8, NOW(), null, NOW());
-
-INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-VALUES ('122', 8, NOW(), null, NOW());
-
-DROP PROCEDURE IF EXISTS insert_navy_seat_sections;
-
-DELIMITER //
-CREATE PROCEDURE insert_navy_seat_sections()
-BEGIN
-    DECLARE i INT DEFAULT 301;
-    WHILE i <= 334 DO
-            INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-            VALUES (CONCAT(i), 9, NOW(), null, NOW());
-            SET i = i + 1;
-        END WHILE;
-END //
-DELIMITER ;
-
-CALL insert_navy_seat_sections();
-
-DROP PROCEDURE IF EXISTS insert_green_seat_sections;
-
-DELIMITER //
-CREATE PROCEDURE insert_green_seat_sections()
-BEGIN
-    DECLARE i INT DEFAULT 401;
-    WHILE i <= 422 DO
-            INSERT INTO seat_section (name, seat_grade_id, created_at, deleted_at, last_updated_at)
-            VALUES (CONCAT(i), 10, NOW(), null, NOW());
-            SET i = i + 1;
-        END WHILE;
-END //
-DELIMITER ;
-
-CALL insert_green_seat_sections();
+CALL insert_seat_sections(6, 205, 208);
+CALL insert_seat_sections(6, 219, 222);
+CALL insert_seat_sections(7, 101, 106);
+CALL insert_seat_sections(7, 201, 204);
+CALL insert_seat_sections(7, 117, 122);
+CALL insert_seat_sections(7, 223, 226);
+CALL insert_seat_sections(8, 101, 102);
+CALL insert_seat_sections(8, 121, 122);
+CALL insert_seat_sections(9, 301, 334);
+CALL insert_seat_sections(10, 401, 422);
