ec65e71957cda60826fd2a0325f4d914b1816166
권성준 <tjdwns1865@naver.com>
2023-05-07 05:15:47 +0900

    Feat: 경기장 상세 조회 API 구현

--- src/main/java/com/goodseats/seatviewreviews/domain/stadium/mapper/StadiumMapper.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/stadium/mapper/StadiumMapper.java
@@ -1,5 +1,6 @@
 package com.goodseats.seatviewreviews.domain.stadium.mapper;
 
+import com.goodseats.seatviewreviews.domain.stadium.model.dto.StadiumDetailResponse;
 import com.goodseats.seatviewreviews.domain.stadium.model.dto.StadiumsElementResponse;
 import com.goodseats.seatviewreviews.domain.stadium.model.entity.Stadium;
 
@@ -11,5 +12,9 @@ public class StadiumMapper {
 
 	public static StadiumsElementResponse toStadiumsElementResponse(Stadium stadium) {
 		return new StadiumsElementResponse(stadium.getId(), stadium.getName(), stadium.getHomeTeam());
+	}
+
+	public static StadiumDetailResponse toStadiumDetailResponse(Stadium stadium) {
+		return new StadiumDetailResponse(stadium.getName(), stadium.getAddress());
 	}
 }
