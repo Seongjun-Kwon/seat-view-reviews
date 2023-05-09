1f5dac717f43630f11a26155403993cf42373d82
권성준 <tjdwns1865@naver.com>
2023-05-10 00:29:33 +0900

    Test: 좌석 목록 조회 API 통합테스트 작성

---
+++ src/test/java/com/goodseats/seatviewreviews/domain/seat/controller/SeatControllerTest.java
@@ -0,0 +1,81 @@
+package com.goodseats.seatviewreviews.domain.seat.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.goodseats.seatviewreviews.domain.seat.mapper.SeatMapper;
+import com.goodseats.seatviewreviews.domain.seat.model.dto.SeatsResponse;
+import com.goodseats.seatviewreviews.domain.seat.model.entity.Seat;
+import com.goodseats.seatviewreviews.domain.seat.model.entity.SeatGrade;
+import com.goodseats.seatviewreviews.domain.seat.model.entity.SeatSection;
+import com.goodseats.seatviewreviews.domain.seat.repository.SeatGradeRepository;
+import com.goodseats.seatviewreviews.domain.seat.repository.SeatRepository;
+import com.goodseats.seatviewreviews.domain.seat.repository.SeatSectionRepository;
+import com.goodseats.seatviewreviews.domain.stadium.model.entity.Stadium;
+import com.goodseats.seatviewreviews.domain.stadium.model.vo.HomeTeam;
+import com.goodseats.seatviewreviews.domain.stadium.repository.StadiumRepository;
+
+@Transactional
+@SpringBootTest
+@AutoConfigureMockMvc
+class SeatControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private StadiumRepository stadiumRepository;
+
+	@Autowired
+	private SeatGradeRepository seatGradeRepository;
+
+	@Autowired
+	private SeatSectionRepository seatSectionRepository;
+
+	@Autowired
+	private SeatRepository seatRepository;
+
+	@Test
+	@DisplayName("Success - 좌석 목록 조회에 성공하고 200 으로 응답한다")
+	void getSeatsSuccess() throws Exception {
+		// given
+		Stadium stadium = new Stadium("잠실 야구장", "서울 송파구 올림픽로 19-2 서울종합운동장", HomeTeam.DOOSAN_LG);
+		SeatGrade seatGrade = new SeatGrade("테이블", "주중 47,000 / 주말 53,000", stadium);
+		SeatSection seatSection = new SeatSection("110", stadium, seatGrade);
+		stadiumRepository.save(stadium);
+		seatGradeRepository.save(seatGrade);
+		seatSectionRepository.save(seatSection);
+
+		Seat seat1 = new Seat("1", seatGrade, seatSection);
+		Seat seat2 = new Seat("2", seatGrade, seatSection);
+		List<Seat> seats = List.of(seat1, seat2);
+		seatRepository.saveAll(seats);
+
+		SeatsResponse seatsResponse = SeatMapper.toSeatsResponse(seats);
+
+		// when & then
+		mockMvc.perform(get("/api/v1/seats")
+						.param("sectionId", seatSection.getId().toString())
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(content().json(objectMapper.writeValueAsString(seatsResponse)))
+				.andDo(print());
+	}
+}
