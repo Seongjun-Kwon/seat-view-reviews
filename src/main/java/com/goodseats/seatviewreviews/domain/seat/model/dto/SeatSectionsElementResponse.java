0ae403dfbac6391b41227c9ba16172dda1ab17c8
권성준 <tjdwns1865@naver.com>
2023-05-09 13:43:13 +0900

    Add: 좌석 구역 목록 응답을 위한 DTO 추가

---
+++ src/main/java/com/goodseats/seatviewreviews/domain/seat/model/dto/SeatSectionsElementResponse.java
@@ -0,0 +1,4 @@
+package com.goodseats.seatviewreviews.domain.seat.model.dto;
+
+public record SeatSectionsElementResponse(Long sectionId, String sectionName) {
+}
---
+++ src/main/java/com/goodseats/seatviewreviews/domain/seat/model/dto/SeatSectionsResponse.java
@@ -0,0 +1,6 @@
+package com.goodseats.seatviewreviews.domain.seat.model.dto;
+
+import java.util.List;
+
+public record SeatSectionsResponse(List<SeatSectionsElementResponse> seatSections) {
+}
