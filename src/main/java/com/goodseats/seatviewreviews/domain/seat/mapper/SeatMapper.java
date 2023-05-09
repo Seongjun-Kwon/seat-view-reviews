7fbaad19d66f40db2fc14852e1d16bb4e81ef7af
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-05-10 00:31:17 +0900

    Feat: 좌석 목록 조회 API 구현 (#53)
    - 좌석 목록 조회 API 구현
    - 좌석 목록 조회 API 관련 테스트 작성

---
+++ src/main/java/com/goodseats/seatviewreviews/domain/seat/mapper/SeatMapper.java
@@ -0,0 +1,22 @@
+package com.goodseats.seatviewreviews.domain.seat.mapper;
+
+import java.util.List;
+
+import com.goodseats.seatviewreviews.domain.seat.model.dto.SeatsElementResponse;
+import com.goodseats.seatviewreviews.domain.seat.model.dto.SeatsResponse;
+import com.goodseats.seatviewreviews.domain.seat.model.entity.Seat;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SeatMapper {
+
+	public static SeatsResponse toSeatsResponse(List<Seat> seats) {
+		List<SeatsElementResponse> elementResponses = seats.stream()
+				.map(seat -> new SeatsElementResponse(seat.getSeatInfo(), seat.getAverageScore()))
+				.toList();
+
+		return new SeatsResponse(elementResponses);
+	}
+}
---
+++ src/main/java/com/goodseats/seatviewreviews/domain/seat/service/SeatService.java
@@ -0,0 +1,26 @@
+package com.goodseats.seatviewreviews.domain.seat.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goodseats.seatviewreviews.domain.seat.mapper.SeatMapper;
+import com.goodseats.seatviewreviews.domain.seat.model.dto.SeatsResponse;
+import com.goodseats.seatviewreviews.domain.seat.model.entity.Seat;
+import com.goodseats.seatviewreviews.domain.seat.repository.SeatRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class SeatService {
+
+	private final SeatRepository seatRepository;
+
+	@Transactional(readOnly = true)
+	public SeatsResponse getSeats(Long sectionId) {
+		List<Seat> seats = seatRepository.findAllBySeatSection(sectionId);
+		return SeatMapper.toSeatsResponse(seats);
+	}
+}
