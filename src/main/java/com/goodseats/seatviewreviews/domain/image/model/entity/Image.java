adb6aa17171670c4b2ca4b0ea40f143f32a24680
권성준 <tjdwns1865@naver.com>
2023-05-16 21:51:57 +0900

    Fix: 이미지 테이블 및 엔티티 수정사항 반영
    - init.sql 테이블 CREATE 문에 IF NOT EXISTS 명령어 추가
    - init.sql 에서 deleted_at 컬럼이 not null 로 되어있던 것을 null 로 수정
    - @Column 어노테이션 빼먹은 것 추가
    - 이미지 객체 생성 시 생성자 파라미터로 savedName 을 받지 않고, imageUrl 에서 추출하도록 함

--- src/main/java/com/goodseats/seatviewreviews/domain/image/model/entity/Image.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/image/model/entity/Image.java
@@ -1,6 +1,9 @@
 package com.goodseats.seatviewreviews.domain.image.model.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -23,21 +26,32 @@ public class Image extends BaseEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(name = "image_type", length = 30, nullable = false)
+	@Enumerated(EnumType.STRING)
 	private ImageType imageType;
 
+	@Column(name = "reference_id", nullable = false)
 	private Long referenceId;
 
+	@Column(name = "image_url", length = 500, nullable = false)
 	private String imageUrl;
 
+	@Column(name = "uploaded_name", length = 50, nullable = false)
 	private String uploadedName;
 
+	@Column(name = "saved_name", length = 50, nullable = false)
 	private String savedName;
 
-	public Image(ImageType imageType, Long referenceId, String imageUrl, String uploadedName, String savedName) {
+	public Image(ImageType imageType, Long referenceId, String imageUrl, String uploadedName) {
 		this.imageType = imageType;
 		this.referenceId = referenceId;
 		this.imageUrl = imageUrl;
 		this.uploadedName = uploadedName;
-		this.savedName = savedName;
+		this.savedName = extractSavedName(imageUrl);
+	}
+
+	private String extractSavedName(String imageUrl) {
+		int beforeSavedNameIndex = imageUrl.lastIndexOf("/");
+		return imageUrl.substring(beforeSavedNameIndex + 1);
 	}
 }
