2a26b598ed31c3773a01ea54f28a8bfdb1940c7e
권성준 <tjdwns1865@naver.com>
2023-05-17 15:42:53 +0900

    Feat: 이미지 외부 저장소와 DB 의 생명주기를 맞추는 로직 구현

---
+++ src/main/java/com/goodseats/seatviewreviews/domain/image/event/RollbackUploadEvent.java
@@ -0,0 +1,13 @@
+package com.goodseats.seatviewreviews.domain.image.event;
+
+import com.goodseats.seatviewreviews.domain.image.model.entity.Image;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public class RollbackUploadEvent {
+
+	private final Image image;
+}
