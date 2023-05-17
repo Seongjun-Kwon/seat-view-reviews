ce1d3a498c950e46ac874a46bed5ecef30707080
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-05-17 15:33:56 +0900

    Hotfix: AuthenticationInterceptor 에서 인증 객체에 담긴 member 가 유효한지 확인 (#69)

--- src/main/java/com/goodseats/seatviewreviews/common/config/WebConfig.java
+++ src/main/java/com/goodseats/seatviewreviews/common/config/WebConfig.java
@@ -5,12 +5,18 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.goodseats.seatviewreviews.common.security.AuthenticationInterceptor;
+import com.goodseats.seatviewreviews.domain.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+
+	private final MemberRepository memberRepository;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new AuthenticationInterceptor());
+		registry.addInterceptor(new AuthenticationInterceptor(memberRepository));
 	}
 }
