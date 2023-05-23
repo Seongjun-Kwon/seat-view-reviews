7b68bd0bc279d75c5657c8ff1557ea183667845e
권성준 <tjdwns1865@naver.com>
2023-05-23 20:53:27 +0900

    Add: BaseEntity 에 delete() 메서드 추가
    - BaseEntity 를 상속하는 엔티티들이 엔티티 삭제 메서드를 갖게하기 위함

--- src/main/java/com/goodseats/seatviewreviews/common/error/exception/ErrorCode.java
+++ src/main/java/com/goodseats/seatviewreviews/common/error/exception/ErrorCode.java
@@ -10,7 +10,8 @@ public enum ErrorCode {
 	DUPLICATED_NICKNAME(409, "중복된 닉네임입니다."),
 	UNAUTHORIZED(401, "인증되지 않은 사용자입니다."),
 	BAD_LOGIN_REQUEST(400, "아이디 혹은 비밀번호가 틀립니다."),
-	BAD_IMAGE_REQUEST(400, "잘못된 이미지 업로드 요청입니다.");
+	BAD_IMAGE_REQUEST(400, "잘못된 이미지 업로드 요청입니다."),
+	ALREADY_DELETED(409, "이미 삭제된 데이터입니다.");
 
 	private final int status;
 	private final String message;
--- src/main/java/com/goodseats/seatviewreviews/domain/BaseEntity.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/BaseEntity.java
@@ -1,6 +1,7 @@
 package com.goodseats.seatviewreviews.domain;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 import javax.persistence.Column;
 import javax.persistence.EntityListeners;
@@ -9,6 +10,9 @@ import javax.persistence.MappedSuperclass;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import com.goodseats.seatviewreviews.common.error.exception.DuplicatedException;
+import com.goodseats.seatviewreviews.common.error.exception.ErrorCode;
 
 import lombok.Getter;
 
@@ -27,4 +31,12 @@ public abstract class BaseEntity {
 
 	@Column(name = "deleted_at", nullable = true)
 	private LocalDateTime deletedAt;
+
+	public void delete() {
+		if (Objects.nonNull(deletedAt)) {
+			throw new DuplicatedException(ErrorCode.ALREADY_DELETED);
+		}
+
+		deletedAt = LocalDateTime.now();
+	}
 }
