4abe69676af75b14bcd567492247a611c3aad8d2
권성준 <tjdwns1865@naver.com>
2023-05-25 17:56:27 +0900

    Test: 이미지 단건 삭제 API 의 통합테스트 작성

--- src/test/java/com/goodseats/seatviewreviews/domain/image/controller/ImageControllerTest.java
+++ src/test/java/com/goodseats/seatviewreviews/domain/image/controller/ImageControllerTest.java
@@ -1,6 +1,7 @@
 package com.goodseats.seatviewreviews.domain.image.controller;
 
 import static com.goodseats.seatviewreviews.common.security.SessionConstant.*;
+import static org.assertj.core.api.AssertionsForClassTypes.*;
 import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
@@ -17,7 +18,9 @@ import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.goodseats.seatviewreviews.domain.image.model.entity.Image;
 import com.goodseats.seatviewreviews.domain.image.model.vo.ImageType;
+import com.goodseats.seatviewreviews.domain.image.repository.ImageRepository;
 import com.goodseats.seatviewreviews.domain.member.model.dto.AuthenticationDTO;
 import com.goodseats.seatviewreviews.domain.member.model.entity.Member;
 import com.goodseats.seatviewreviews.domain.member.model.vo.MemberAuthority;
@@ -33,6 +36,9 @@ class ImageControllerTest {
 
 	@Autowired
 	private MemberRepository memberRepository;
+
+	@Autowired
+	private ImageRepository imageRepository;
 
 	@Test
 	@DisplayName("Success - 이미지 저장에 성공하고 200 으로 응답한다")
@@ -116,6 +122,74 @@ class ImageControllerTest {
 							.accept(APPLICATION_JSON))
 					.andExpect(status().isUnauthorized())
 					.andDo(print());
+		}
+	}
+
+	@Test
+	@DisplayName("Success - 이미지 단건 삭제에 성공하고 204 응답한다")
+	void deleteImageSuccess() throws Exception {
+		// given
+		Member member = new Member("test@test.com", "test", "test");
+		memberRepository.save(member);
+
+		AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+		MockHttpSession session = new MockHttpSession();
+		session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
+
+		Image image = new Image(ImageType.REVIEW, 1L, "testUrl", "테스트 이미지.jpg");
+		Image savedImage = imageRepository.save(image);
+
+		// when
+		mockMvc.perform(delete("/api/v1/images/{imageId}", savedImage.getId())
+						.session(session))
+				.andExpect(status().isNoContent());
+
+		// then
+		assertThat(savedImage.getDeletedAt()).isNotNull();
+	}
+
+	@Nested
+	@DisplayName("deleteImageFail")
+	class DeleteImage {
+
+		@Test
+		@DisplayName("Fail - 삭제하고자 하는 이미지가 존재하지 않으면 실패하고 404 응답한다")
+		void deleteImageFailByNotFound() throws Exception {
+			// given
+			Member member = new Member("test@test.com", "test", "test");
+			memberRepository.save(member);
+
+			AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+			MockHttpSession session = new MockHttpSession();
+			session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
+
+			Long imageId = 1L;
+
+			// when & then
+			mockMvc.perform(delete("/api/v1/images/{imageId}", imageId)
+							.session(session))
+					.andExpect(status().isNotFound());
+		}
+
+		@Test
+		@DisplayName("Fail - 삭제하고자 하는 이미지가 이미 삭제되었으면 실패하고 409 응답한다")
+		void deleteImageFailByAlreadyDeleted() throws Exception {
+			// given
+			Member member = new Member("test@test.com", "test", "test");
+			memberRepository.save(member);
+
+			AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+			MockHttpSession session = new MockHttpSession();
+			session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
+
+			Image image = new Image(ImageType.REVIEW, 1L, "testUrl", "테스트 이미지.jpg");
+			Image savedImage = imageRepository.save(image);
+			image.delete();
+
+			// when & then
+			mockMvc.perform(delete("/api/v1/images/{imageId}", savedImage.getId())
+							.session(session))
+					.andExpect(status().isConflict());
 		}
 	}
 }
