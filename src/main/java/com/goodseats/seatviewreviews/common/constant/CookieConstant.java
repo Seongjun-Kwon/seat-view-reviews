e224e47477abe20c0b898eaba450fc0e796ed357
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-06-13 03:25:07 +0900

    Feat: 조회 수 중복 방지를 위한 쿠키 로직 구현 (#110)
    - 이미 조회 수 중복 방지 식별 쿠키가 있으면 그것을 사용하고, 없으면 새로 만듬
    - 회원일 경우에도 회원을 식별해주는 JSESSIONID 와는 별도로 새로운 쿠키를 사용함
      - 로그인, 로그아웃을 통해 비회원->회원, 회원->비회원 과정을 거쳐도 조회 수 중복이 되지 않게 하기 위해

---
+++ src/main/java/com/goodseats/seatviewreviews/common/constant/CookieConstant.java
@@ -0,0 +1,10 @@
+package com.goodseats.seatviewreviews.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class CookieConstant {
+
+	public static final String USER_KEY = "USER_KEY";
+}
