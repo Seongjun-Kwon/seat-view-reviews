265b89726eeb91cc9862419c4e097ff6f679effc
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-06-13 02:48:47 +0900

    Chore: 테스트 환경 DB 분리 (#108)
    - 테스트 환경에서 사용할 application-test.yml 추가하고 DB 가 필요한 테스트에서 프로파일을 test 로 명시함
    - CI/CD 과정에서 테스트 DB 용 env 파일을 분리함

--- src/test/java/com/goodseats/seatviewreviews/domain/seat/controller/SeatControllerTest.java
+++ src/test/java/com/goodseats/seatviewreviews/domain/seat/controller/SeatControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,6 +29,7 @@ import com.goodseats.seatviewreviews.domain.stadium.model.entity.Stadium;
 import com.goodseats.seatviewreviews.domain.stadium.model.vo.HomeTeam;
 import com.goodseats.seatviewreviews.domain.stadium.repository.StadiumRepository;
 
+@ActiveProfiles("test")
 @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
