69bb2df2ffa6ca21917bab8382fb8e7a25c8ab0d
권성준 <tjdwns1865@naver.com>
2023-07-02 17:44:47 +0900

    Test: 후기 상세 조회 API 컨트롤러 통합테스트 작성

--- src/test/java/com/goodseats/seatviewreviews/domain/review/controller/ReviewControllerTest.java
+++ src/test/java/com/goodseats/seatviewreviews/domain/review/controller/ReviewControllerTest.java
@@ -1,9 +1,15 @@
 package com.goodseats.seatviewreviews.domain.review.controller;
 
+import static com.goodseats.seatviewreviews.common.constant.CookieConstant.*;
 import static com.goodseats.seatviewreviews.common.security.SessionConstant.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.UUID;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -11,10 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockCookie;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.goodseats.seatviewreviews.domain.member.model.dto.AuthenticationDTO;
@@ -25,6 +31,7 @@ import com.goodseats.seatviewreviews.domain.review.model.dto.request.ReviewPubli
 import com.goodseats.seatviewreviews.domain.review.model.dto.request.TempReviewCreateRequest;
 import com.goodseats.seatviewreviews.domain.review.model.entity.Review;
 import com.goodseats.seatviewreviews.domain.review.repository.ReviewRepository;
+import com.goodseats.seatviewreviews.domain.review.service.ReviewRedisFacade;
 import com.goodseats.seatviewreviews.domain.seat.model.entity.Seat;
 import com.goodseats.seatviewreviews.domain.seat.model.entity.SeatGrade;
 import com.goodseats.seatviewreviews.domain.seat.model.entity.SeatSection;
@@ -36,7 +43,7 @@ import com.goodseats.seatviewreviews.domain.stadium.model.vo.HomeTeam;
 import com.goodseats.seatviewreviews.domain.stadium.repository.StadiumRepository;
 
 @ActiveProfiles("test")
-@Transactional
+// @Transactional
 @SpringBootTest
 @AutoConfigureMockMvc
 class ReviewControllerTest {
@@ -46,6 +53,9 @@ class ReviewControllerTest {
 
 	@Autowired
 	private ObjectMapper objectMapper;
+
+	@Autowired
+	private ReviewRedisFacade reviewRedisFacade;
 
 	@Autowired
 	private MemberRepository memberRepository;
@@ -65,23 +75,52 @@ class ReviewControllerTest {
 	@Autowired
 	private ReviewRepository reviewRepository;
 
-	@Test
-	@DisplayName("Success - 후기 임시 생성에 성공하고 200 응답한다")
-	void createTempReviewSuccess() throws Exception {
-		// given
-		Member member = new Member("test@test.com", "test", "test");
-		Stadium stadium = new Stadium("잠실 야구장", "서울 송파구 올림픽로 19-2 서울종합운동장", HomeTeam.DOOSAN_LG);
-		SeatGrade seatGrade = new SeatGrade("테이블", "주중 47,000 / 주말 53,000", stadium);
-		SeatSection seatSection = new SeatSection("110", stadium, seatGrade);
-		Seat seat = new Seat("1", seatGrade, seatSection);
+	private Member writer;
+	private Member notWriter;
+	private Stadium stadium;
+	private SeatGrade seatGrade;
+	private SeatSection seatSection;
+	private Seat seat;
+	private Review tempReview;
+	private Review publishedReview;
 
-		memberRepository.save(member);
+	@BeforeEach
+	void setUp() {
+		writer = new Member("writer@test.com", "test", "writer");
+		notWriter = new Member("notWriter@test.com", "test", "notWriter");
+		stadium = new Stadium("잠실 야구장", "서울 송파구 올림픽로 19-2 서울종합운동장", HomeTeam.DOOSAN_LG);
+		seatGrade = new SeatGrade("테이블", "주중 47,000 / 주말 53,000", stadium);
+		seatSection = new SeatSection("110", stadium, seatGrade);
+		seat = new Seat("1", seatGrade, seatSection);
+		tempReview = new Review(writer, seat);
+		publishedReview = new Review("테스트 제목", "테스트 내용", 5, writer, seat);
+
+		memberRepository.save(writer);
+		memberRepository.save(notWriter);
 		stadiumRepository.save(stadium);
 		seatGradeRepository.save(seatGrade);
 		seatSectionRepository.save(seatSection);
 		seatRepository.save(seat);
+		reviewRepository.save(tempReview);
+		reviewRepository.save(publishedReview);
+	}
 
-		AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+	@AfterEach
+	void clear() {
+		memberRepository.delete(writer);
+		memberRepository.delete(notWriter);
+		stadiumRepository.delete(stadium);
+		seatGradeRepository.delete(seatGrade);
+		seatSectionRepository.delete(seatSection);
+		seatRepository.delete(seat);
+		reviewRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("Success - 후기 임시 생성에 성공하고 200 응답한다")
+	void createTempReviewSuccess() throws Exception {
+		// given
+		AuthenticationDTO authenticationDTO = new AuthenticationDTO(writer.getId(), MemberAuthority.USER);
 		MockHttpSession session = new MockHttpSession();
 		session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
 
@@ -101,11 +140,8 @@ class ReviewControllerTest {
 	void createTempReviewFailByNotFoundSeatId() throws Exception {
 		// given
 		Long wrongSeatId = 0L;
-		Member member = new Member("test@test.com", "test", "test");
 
-		memberRepository.save(member);
-
-		AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+		AuthenticationDTO authenticationDTO = new AuthenticationDTO(writer.getId(), MemberAuthority.USER);
 		MockHttpSession session = new MockHttpSession();
 		session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
 
@@ -124,21 +160,7 @@ class ReviewControllerTest {
 	@DisplayName("Success - 후기 발행에 성공하고 204 응답한다")
 	void publishReviewSuccess() throws Exception {
 		// given
-		Member member = new Member("test@test.com", "test", "test");
-		Stadium stadium = new Stadium("잠실 야구장", "서울 송파구 올림픽로 19-2 서울종합운동장", HomeTeam.DOOSAN_LG);
-		SeatGrade seatGrade = new SeatGrade("테이블", "주중 47,000 / 주말 53,000", stadium);
-		SeatSection seatSection = new SeatSection("110", stadium, seatGrade);
-		Seat seat = new Seat("1", seatGrade, seatSection);
-		Review tempReview = new Review(member, seat);
-
-		memberRepository.save(member);
-		stadiumRepository.save(stadium);
-		seatGradeRepository.save(seatGrade);
-		seatSectionRepository.save(seatSection);
-		seatRepository.save(seat);
-		reviewRepository.save(tempReview);
-
-		AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+		AuthenticationDTO authenticationDTO = new AuthenticationDTO(writer.getId(), MemberAuthority.USER);
 		MockHttpSession session = new MockHttpSession();
 		session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
 
@@ -162,11 +184,8 @@ class ReviewControllerTest {
 		@DisplayName("Fail - 발행하고자 하는 임시 후기가 존재하지 않으면 실패하고 404 응답한다")
 		void publishReviewFailByNotFoundTempReview() throws Exception {
 			Long wrongReviewId = 1L;
-			Member member = new Member("test@test.com", "test", "test");
 
-			memberRepository.save(member);
-
-			AuthenticationDTO authenticationDTO = new AuthenticationDTO(member.getId(), MemberAuthority.USER);
+			AuthenticationDTO authenticationDTO = new AuthenticationDTO(writer.getId(), MemberAuthority.USER);
 			MockHttpSession session = new MockHttpSession();
 			session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
 
@@ -185,23 +204,8 @@ class ReviewControllerTest {
 		@Test
 		@DisplayName("Fail - 발행하는 회원이 임시 후기를 작성한 회원이 아니면 실패하고 401 응답한다")
 		void publishReviewFailByNotTempReviewWriter() throws Exception {
-			Member writerMember = new Member("writer@test.com", "test", "writer");
-			Member notWriterMember = new Member("notWriter@test.com", "test", "notWriter");
-			Stadium stadium = new Stadium("잠실 야구장", "서울 송파구 올림픽로 19-2 서울종합운동장", HomeTeam.DOOSAN_LG);
-			SeatGrade seatGrade = new SeatGrade("테이블", "주중 47,000 / 주말 53,000", stadium);
-			SeatSection seatSection = new SeatSection("110", stadium, seatGrade);
-			Seat seat = new Seat("1", seatGrade, seatSection);
-			Review tempReview = new Review(writerMember, seat);
-
-			memberRepository.save(writerMember);
-			memberRepository.save(notWriterMember);
-			stadiumRepository.save(stadium);
-			seatGradeRepository.save(seatGrade);
-			seatSectionRepository.save(seatSection);
-			seatRepository.save(seat);
-			reviewRepository.save(tempReview);
-
-			AuthenticationDTO authenticationDTO = new AuthenticationDTO(notWriterMember.getId(), MemberAuthority.USER);
+			// given
+			AuthenticationDTO authenticationDTO = new AuthenticationDTO(notWriter.getId(), MemberAuthority.USER);
 			MockHttpSession session = new MockHttpSession();
 			session.setAttribute(LOGIN_MEMBER_INFO, authenticationDTO);
 
@@ -216,5 +220,59 @@ class ReviewControllerTest {
 							.param("score", String.valueOf(reviewPublishRequest.score())))
 					.andExpect(status().isUnauthorized());
 		}
+	}
+
+	@Nested
+	@DisplayName("getReviewSuccess")
+	class getReviewSuccess {
+		@Test
+		@DisplayName("Success - 처음 조회하는 후기의 상세 조회에 성공하고 200 응답한다")
+		void getReviewSuccessWhenFirstRead() throws Exception {
+			// given & when & then
+			mockMvc.perform(get("/api/v1/reviews/{reviewId}", publishedReview.getId())
+							.accept(MediaType.APPLICATION_JSON))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("title").value(publishedReview.getTitle()))
+					.andExpect(jsonPath("content").value(publishedReview.getContent()))
+					.andExpect(jsonPath("score").value(publishedReview.getScore()))
+					.andExpect(jsonPath("viewCount").value(publishedReview.getViewCount()))
+					.andExpect(jsonPath("writer").value(publishedReview.getMember().getNickname()))
+					.andExpect(cookie().exists(USER_KEY))
+					.andDo(print());
+		}
+
+		@Test
+		@DisplayName("Success - 이미 조회한 후기의 상세 조회에 성공하고 200 응답한다")
+		void getReviewSuccessWhenAlreadyRead() throws Exception {
+			// given
+			String userCookieValue = String.valueOf(UUID.randomUUID());
+			MockCookie cookie = new MockCookie(USER_KEY, userCookieValue);
+
+			// when & then
+			mockMvc.perform(get("/api/v1/reviews/{reviewId}", publishedReview.getId())
+							.accept(MediaType.APPLICATION_JSON)
+							.cookie(cookie))
+					.andExpect(status().isOk())
+					.andExpect(jsonPath("title").value(publishedReview.getTitle()))
+					.andExpect(jsonPath("content").value(publishedReview.getContent()))
+					.andExpect(jsonPath("score").value(publishedReview.getScore()))
+					.andExpect(jsonPath("viewCount").value(publishedReview.getViewCount()))
+					.andExpect(jsonPath("writer").value(publishedReview.getMember().getNickname()))
+					.andExpect(cookie().value(USER_KEY, userCookieValue))
+					.andDo(print());
+		}
+	}
+
+	@Test
+	@DisplayName("Fail - 조회하려는 후기 id 가 없으면 실패하고 404 응답한다")
+	void getReviewFailByNotFound() throws Exception {
+		// given
+		Long wrongReviewId = -1L;
+
+		// when & then
+		mockMvc.perform(get("/api/v1/reviews/{reviewId}", wrongReviewId)
+						.accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isNotFound())
+				.andDo(print());
 	}
 }
