c2dd53c8a558338106d2f4ff3ec755a8298494cd
권성준 <tjdwns1865@naver.com>
2023-07-04 00:39:35 +0900

    Feat: 후기 조회 수 스케줄링 로직 구현
    - 조회 수 배치 처리
      - DB 에 부하가 많이 갈 것으로 예상하여 실시간으로 DB 에 조회 수를 반영하지 않음
        - 특히, 사용자가 F5 계속 누르고 있는 경우와 같이 악의적인 공격을 예방하기 위함
      - 조회 수는 실시간성도 중요한 값이기에 5분마다 조회 수를 업데이트 해줌
      - Redis 에 저장된 데이터는 인기 글 산정 주기인 하루마다 제거

---
+++ src/main/java/com/goodseats/seatviewreviews/common/constant/SchedulerConstant.java
@@ -0,0 +1,10 @@
+package com.goodseats.seatviewreviews.common.constant;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SchedulerConstant {
+
+	public static final int VIEW_COUNT_SCHEDULING_MINUTE = 5;
+}
