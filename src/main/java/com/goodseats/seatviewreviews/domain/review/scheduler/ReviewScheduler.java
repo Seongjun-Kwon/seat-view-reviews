c0b2108e11d8482b1b284922632bf43826b44d37
Seongjun Kwon <82152173+Seongjun-Kwon@users.noreply.github.com>
2023-07-05 05:11:25 +0900

    Feat: 후기 조회 수 스케줄링 로직 구현 (#122)
    - 조회 수 배치 처리
      - DB 에 부하가 많이 갈 것으로 예상하여 실시간으로 DB 에 조회 수를 반영하지 않음
        - 특히, 사용자가 F5 계속 누르고 있는 경우와 같이 악의적인 공격을 예방하기 위함
      - 조회 수는 실시간성도 중요한 값이기에 5분마다 조회 수를 업데이트 해줌
      - Redis 에 저장된 데이터는 인기 글 산정 주기인 하루마다 제거
    
    - 스케줄링 로직 테스트 작성
      - 스케줄링 동작 자체는 Spring 의 동작이므로 테스트하지 않음
      - 스케줄러 메서드 호출 시 의도한 대로 동작하는지 Mockito 이용하여 테스트함

---
+++ src/main/java/com/goodseats/seatviewreviews/domain/review/scheduler/ReviewScheduler.java
@@ -0,0 +1,99 @@
+package com.goodseats.seatviewreviews.domain.review.scheduler;
+
+import static com.goodseats.seatviewreviews.common.constant.RedisConstant.*;
+import static com.goodseats.seatviewreviews.common.constant.SchedulerConstant.*;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import org.redisson.api.RMap;
+import org.redisson.api.RedissonClient;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.goodseats.seatviewreviews.common.error.exception.ErrorCode;
+import com.goodseats.seatviewreviews.common.error.exception.NotFoundException;
+import com.goodseats.seatviewreviews.domain.review.model.entity.Review;
+import com.goodseats.seatviewreviews.domain.review.repository.ReviewRepository;
+import com.goodseats.seatviewreviews.domain.review.service.ReviewRedisFacade;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class ReviewScheduler {
+
+	private final ReviewRedisFacade reviewRedisFacade;
+	private final RedissonClient redissonClient;
+	private final ReviewRepository reviewRepository;
+
+	@Transactional
+	@Scheduled(cron = "0 */5 * * * *")
+	public void syncViewCountToDB() {
+		RMap<String, String> reviewAndViewCountLogs = redissonClient.getMap(REVIEW_AND_VIEW_COUNT_LOGS_NAME);
+		String previousScheduledMinute = getPreviousScheduledMinute();
+		List<String> targetKeys = getTargetsToSync(previousScheduledMinute, reviewAndViewCountLogs);
+		updateViewCount(reviewAndViewCountLogs, targetKeys);
+	}
+
+	@Scheduled(cron = "0 0 0 * * *")
+	public void clearViewCountLogs() {
+		reviewRedisFacade.clearAllLogs();
+	}
+
+	private String getPreviousScheduledMinute() {
+		LocalDateTime now = LocalDateTime.now();
+		LocalDateTime previousScheduledMinute = now.minusMinutes(VIEW_COUNT_SCHEDULING_MINUTE);
+		return previousScheduledMinute.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+
+	private List<String> getTargetsToSync(String previousScheduledMinute, RMap<String, String> reviewAndViewCountLogs) {
+		return reviewAndViewCountLogs.keySet().stream()
+				.filter(key -> isTargetToSync(previousScheduledMinute, key))
+				.collect(Collectors.toMap(
+						this::extractReviewId,
+						Function.identity(),
+						BinaryOperator.maxBy(Comparator.comparing(this::extractViewedTime))
+				))
+				.values()
+				.stream()
+				.toList();
+	}
+
+	private boolean isTargetToSync(String previousScheduledMinute, String key) {
+		int beforeViewedTimeIndex = key.lastIndexOf(DELIMITER);
+		String viewedTime = key.substring(beforeViewedTimeIndex + 1);
+		return viewedTime.compareTo(previousScheduledMinute) >= 0;
+	}
+
+	private String extractReviewId(String key) {
+		int beforeReviewIdIndex = key.indexOf(DELIMITER);
+		int afterReviewIdIndex = key.indexOf(SEPARATOR);
+		return key.substring(beforeReviewIdIndex + 1, afterReviewIdIndex);
+	}
+
+	private LocalDateTime extractViewedTime(String key) {
+		int beforeTimeIndex = key.lastIndexOf(DELIMITER);
+		String timeString = key.substring(beforeTimeIndex + 1);
+		return LocalDateTime.parse(timeString, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+	}
+
+	private void updateViewCount(RMap<String, String> reviewAndViewCountLogs, List<String> targetKeys) {
+		targetKeys
+				.forEach(key -> {
+					int viewCount = Integer.parseInt(reviewAndViewCountLogs.get(key));
+					Long reviewId = Long.valueOf(extractReviewId(key));
+
+					Review review = reviewRepository.findById(reviewId)
+							.orElseThrow(() -> new NotFoundException(ErrorCode.NOT_FOUND));
+
+					review.updateViewCount(viewCount);
+				});
+	}
+}
