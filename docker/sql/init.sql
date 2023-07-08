a8d6cf89ed7d854fb99120e905f8eff335ab63e8
권성준 <tjdwns1865@naver.com>
2023-07-09 04:57:51 +0900

    Style: 후기의 발행 여부를 나타내는 필드 명을 isPublished 에서 published 로 수정

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -76,8 +76,8 @@ CREATE TABLE IF NOT EXISTS `review`
     `title`           VARCHAR(50)    NULL,
     `content`         VARCHAR(10000) NULL,
     `score`           INT            NULL,
-    `view_count`            INT            NOT NULL,
-    `is_published`    BOOLEAN        NOT NULL,
+    `view_count`      INT            NOT NULL,
+    `published`       BOOLEAN        NOT NULL,
     `created_at`      TIMESTAMP      NOT NULL,
     `last_updated_at` TIMESTAMP      NOT NULL,
     `deleted_at`      TIMESTAMP      NULL
