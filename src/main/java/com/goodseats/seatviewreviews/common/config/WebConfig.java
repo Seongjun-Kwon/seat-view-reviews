ca7473dbf5a6fe6f871f004a3c1191ceaefbf512
권성준 <tjdwns1865@naver.com>
2023-08-07 01:19:01 +0900

    Fix: AuthenticationInterceptor 에서 유저 정보를 DB 에 확인하는 단계 제거
    - 로그인이 필요한 매 요청마다 DB 에 확인할 필요가 없다. 세션의 유저 정보 유무로 인증/인가를 판별하는 것이 옳다.

--- src/main/java/com/goodseats/seatviewreviews/common/config/WebConfig.java
+++ src/main/java/com/goodseats/seatviewreviews/common/config/WebConfig.java
@@ -5,7 +5,6 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.goodseats.seatviewreviews.common.security.AuthenticationInterceptor;
-import com.goodseats.seatviewreviews.domain.member.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -13,11 +12,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final MemberRepository memberRepository;
-
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(new AuthenticationInterceptor(memberRepository))
+		registry.addInterceptor(new AuthenticationInterceptor())
 				.addPathPatterns("/api/**");
 	}
 }
