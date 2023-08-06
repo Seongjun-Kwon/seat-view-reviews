e0ebfb435ba053c518ded32b40c678986f9defd4
권성준 <tjdwns1865@naver.com>
2023-08-07 01:17:46 +0900

    Fix: 후기 투표 생성 시 회원 id 를 세션에서 얻도록 수정

--- src/main/java/com/goodseats/seatviewreviews/domain/vote/model/dto/request/ReviewVoteCreateRequest.java
+++ src/main/java/com/goodseats/seatviewreviews/domain/vote/model/dto/request/ReviewVoteCreateRequest.java
@@ -5,7 +5,6 @@ import javax.validation.constraints.NotNull;
 import com.goodseats.seatviewreviews.domain.vote.model.vo.VoteChoice;
 
 public record ReviewVoteCreateRequest(
-		@NotNull Long memberId,
 		@NotNull Long reviewId,
 		@NotNull VoteChoice voteChoice
 ) {
