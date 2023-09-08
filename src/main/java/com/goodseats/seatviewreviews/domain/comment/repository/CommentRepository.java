6f214ae99b9870c414a18064ed7c722c4548bf50
권성준 <tjdwns1865@naver.com>
2023-09-08 21:23:16 +0900

    Chore: 불필요한 파일 제거

--- src/main/java/com/goodseats/seatviewreviews/domain/comment/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.goodseats.seatviewreviews.domain.comment.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.goodseats.seatviewreviews.domain.comment.model.entity.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-
-}
